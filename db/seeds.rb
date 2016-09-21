81f4efa5ce345308b742c410121bd93f95ad5584
quoc hung <tranquochung.18892@gmail.com>
2016-09-21 14:24:27 +0700

    Add user following

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,8 +20,17 @@ User.create!(name:  "Example User",
                password:              password,
                password_confirmation: password)
 end
+#entries
 users = User.order(:created_at).take(6)
 50.times do
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.entries.create!(content: content) }
 end
+
+# Following relationships
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
